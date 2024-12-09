e25c4810342087f363fc5b17b37494fbb1a1e897
huijee <hpwhj0815@naver.com>
2024-12-09 13:51:14 +0900

    update) 비밀번호 찾기 및 만료 시 모달 생성(메인인증 오류 수정 중..)

--- src/main/java/com/hjph/login/config/MailConfig.java
+++ src/main/java/com/hjph/login/config/MailConfig.java
@@ -33,13 +33,13 @@ public class MailConfig {
     @Value("${spring.mail.properties.mail.smtp.starttls.required}")
     private boolean starttlsRequired;
 
-    @Value("${spring.mail.properties.mail.smtp.connectiontimeout}")
+    @Value("${spring.mail.properties.mail.smtp.connection-timeout}")
     private int connectionTimeout;
 
     @Value("${spring.mail.properties.mail.smtp.timeout}")
     private int timeout;
 
-    @Value("${spring.mail.properties.mail.smtp.writetimeout}")
+    @Value("${spring.mail.properties.mail.smtp.write-timeout}")
     private int writeTimeout;
 
     @Bean
@@ -53,9 +53,9 @@ public class MailConfig {
         properties.put("mail.smtp.auth", auth);
         properties.put("mail.smtp.starttls.enable", starttlsEnable);
         properties.put("mail.smtp.starttls.required", starttlsRequired);
-        properties.put("mail.smtp.connectiontimeout", connectionTimeout);
+        properties.put("mail.smtp.connection-timeout", connectionTimeout);
         properties.put("mail.smtp.timeout", timeout);
-        properties.put("mail.smtp.writetimeout", writeTimeout);
+        properties.put("mail.smtp.write-timeout", writeTimeout);
         return properties;
     }
 
--- src/main/java/com/hjph/login/service/MailSettingService.java
+++ src/main/java/com/hjph/login/service/MailSettingService.java
@@ -28,45 +28,11 @@ public class MailSettingService {
         mailSender.send(message);
     }
 
-    public SimpleMailMessage createEmail(String toEmail, String title, String text) {
+    public SimpleMailMessage createEmailForm(String toEmail, String title, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(toEmail);
         message.setSubject(title);
         message.setText(text);
         return message;
     }
-
-
-
-//    public String sendMail(String email) {
-//
-//        Random r = new Random();
-//        int checkNum = r.nextInt(888888) + 111111;
-//
-//        String title = "비밀번호 찾기 인증 이메일입니다.";
-//        String to = email;
-//        String content =
-//                System.lineSeparator() +
-//                System.lineSeparator() +
-//                        "안녕하세요. Nizy를 방문해주셔서 감사합니다." +
-//                        System.lineSeparator() +
-//                        "요청하신 인증번호는 " + checkNum + "입니다." +
-//                        System.lineSeparator();
-//
-//        try {
-//            MimeMessage message = mailSender.createMimeMessage();
-//            MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
-//
-//            messageHelper.setFrom(from);
-//            messageHelper.setTo(to);
-//            messageHelper.setSubject(title);
-//            messageHelper.setText(content, true);
-//
-//            mailSender.send(message);
-//        } catch (MessagingException e) {
-//            e.printStackTrace();
-//        }
-//        return checkNum+"";
-//    }
-
 }
