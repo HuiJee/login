d8bf77876cbb95149e1709cb9af4151b82c1c87e
huijee <hpwhj0815@naver.com>
2024-12-13 14:46:31 +0900

    update) Redis에 RefreshToken 저장 성공 / 로그인 후 결과 페이지 경로 수정

--- src/main/java/com/hjpj/login/repository/RedisRepository.java
+++ src/main/java/com/hjpj/login/repository/RedisRepository.java
@@ -4,14 +4,8 @@ import com.hjpj.login.entity.TokenRedis;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface RedisRepository extends CrudRepository<TokenRedis, String> {
-
-    Optional<TokenRedis> findByAccessToken(String accessToken);
-
-    Optional<TokenRedis> findByUserLogId(String userLogId);
 
     void deleteByUserLogId(String userLogId);
 
